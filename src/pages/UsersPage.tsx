424a0fa5c58e29eb854e0c08256431fb33b27d03
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-24 12:06:43 +0300

    photos connection

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,43 +1,49 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-interface UsersData {
-  users?: number[];
+interface User {
+  name: string;
+  avatar: string;
 }
-const UsersPage: React.FC = () => {
-    const [usersData, setUsersData] = useState<UsersData>({});
-  
-    useEffect(() => {
-           fetch("https://birthdayquestbackend.onrender.com/users")
-             .then(response => response.json())
-             .then(data => {
-               setUsersData(data);
-             });
-         }, []);
-      
-  
-    useEffect(() => {
-      window.scrollTo(0, 0)
-    }, [])
 
-    return (
-      <div className="usersPage">
-        <div className="pageTitle">
-            This is the users page.
-        </div>
-        <div className="text">
-            Some text here
-        </div>
-        <div>
-          {typeof usersData.users === 'undefined' ? (
-            <p>Loading...</p>
-          ) : (
-            usersData.users.map((user, i) => (
-              <p key={i}>{user}</p>
-            ))
-          )}
-        </div>
+const UsersPage: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("https://birthdayquestbackend.onrender.com/users");
+        setUsers(response.data.users);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="usersPage">
+      <div className="pageTitle">
+        This is the users page.
+      </div>
+      <div className="text">
+        Some text here
+      </div>
+      <div>
+        {users.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          users.map((user, index) => (
+            <div key={index} className="user">
+              <p>{user.name}</p>
+              <img src={`https://birthdayquestbackend.onrender.com/avatars/${user.avatar}`} alt={`${user.name}'s avatar`} style={{height: '200px'}}/>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-    );
-  };
+  );
+};
 
 export default UsersPage;
