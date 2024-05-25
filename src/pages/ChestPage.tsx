d5f639253155b8c18837bdb3408ae2d5df544fb0
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-25 15:11:50 +0300

    Search added

--- src/pages/ChestPage.tsx
+++ src/pages/ChestPage.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 interface Champion {
   name: string;
   chest: string;
 }
 
+interface ChampionLol {
+  name: string;
+  chest: string;
+}
+
 const ChestPage: React.FC = () => {
   const [champions, setChampions] = useState<Champion[]>([]);
+  const [championsLol, setChampionsLol] = useState<ChampionLol[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
@@ -31,6 +39,27 @@ const ChestPage: React.FC = () => {
     fetchChampions();
   }, []);
 
+  useEffect(() => {
+    const fetchChampionsLol = async () => {
+      try {
+        const response = await axios.get("https://birthdayquestbackend.onrender.com/champions_lol");
+        setChampionsLol(response.data.champions_lol);
+      } catch (error) {
+        console.error('Error fetching champions_lol:', error);
+      }
+    };
+
+    fetchChampionsLol();
+  }, []);
+
+  const options = championsLol.map((championLol) => {
+    const firstLetter = championLol.name[0].toUpperCase();
+    return {
+      firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+      ...championLol,
+    };
+  });
+
   return (
     <div className="chestsPage">
       <div className="pageTitle">
@@ -40,15 +69,9 @@ const ChestPage: React.FC = () => {
         Some text here
       </div>
       <div>
-        {champions.length === 0 ? (
+        {champions.length === 0  && championsLol.length === 0 ? (
           <p>Loading...</p>
         ) : (
-          // champions.map((champion, index) => (
-          //   <div key={index} className="champion">
-          //     <img src={`https://birthdayquestbackend.onrender.com/chests/${champion.chest}`} alt={`${champion.name}'s chest`} style={{height: '200px'}}/>
-          //     <p>{champion.name}</p>
-          //   </div>
-          // ))
           currentIndex < champions.length ? (
           <div className="champion">
             <img 
@@ -57,8 +80,24 @@ const ChestPage: React.FC = () => {
               style={{ height: '200px' }} 
             />
             <p>{champions[currentIndex].name}</p>
+            <Autocomplete
+              id="championsLolSearch"
+              options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+              groupBy={(option) => option.firstLetter}
+              getOptionLabel={(option) => option.name}
+              // sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Чемпион" />}
+            />
           </div>) : 
-          <p>Done</p>
+          championsLol.length === 0 ? (
+            <p>Loading...</p>
+          ) :
+          // <p>Done</p>
+          championsLol.map((championLol, index) => (
+            <div key={index} className="championLol">
+            <p>{championLol.name}</p>
+          </div>
+          ))
         )}
       </div>
       {champions.length > 0 && (
