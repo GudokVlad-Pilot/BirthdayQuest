9ebacc7550d3f49c15e9aa398eb70241a66b86c6
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-25 13:43:44 +0300

    step by step

--- src/pages/ChestPage.tsx
+++ src/pages/ChestPage.tsx
@@ -8,6 +8,11 @@ interface Champion {
 
 const ChestPage: React.FC = () => {
   const [champions, setChampions] = useState<Champion[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex+1);
+  };
 
   useEffect(() => {
     const fetchChampions = async () => {
@@ -34,14 +39,27 @@ const ChestPage: React.FC = () => {
         {champions.length === 0 ? (
           <p>Loading...</p>
         ) : (
-          champions.map((champion, index) => (
-            <div key={index} className="champion">
-              <img src={`https://birthdayquestbackend.onrender.com/chests/${champion.chest}`} alt={`${champion.name}'s chest`} style={{height: '200px'}}/>
-              <p>{champion.name}</p>
-            </div>
-          ))
+          // champions.map((champion, index) => (
+          //   <div key={index} className="champion">
+          //     <img src={`https://birthdayquestbackend.onrender.com/chests/${champion.chest}`} alt={`${champion.name}'s chest`} style={{height: '200px'}}/>
+          //     <p>{champion.name}</p>
+          //   </div>
+          // ))
+          currentIndex < champions.length ? (
+          <div className="champion">
+            <img 
+              src={`https://birthdayquestbackend.onrender.com/chests/${champions[currentIndex].chest}`} 
+              alt={`${champions[currentIndex].name}'s chest`} 
+              style={{ height: '200px' }} 
+            />
+            <p>{champions[currentIndex].name}</p>
+          </div>) : 
+          <p>Done</p>
         )}
       </div>
+      {champions.length > 0 && (
+        <button onClick={handleNext}>Далее</button>
+      )}
     </div>
   );
 };
