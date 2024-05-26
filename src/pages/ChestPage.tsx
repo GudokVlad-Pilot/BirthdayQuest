15ce4eda2a1ac236021be198431c6c359912496a
Vladislav Pogudin <bvlpo001@edu.xamk.fi>
2024-05-27 02:12:33 +0300

    fix for colors

--- src/pages/ChestPage.tsx
+++ src/pages/ChestPage.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { red } from "@mui/material/colors";
 
 interface Champion {
   name: string;
